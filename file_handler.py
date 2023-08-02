4808767f95eab51e6a04b5fc97b213fb7422d6bc
JC Park <whdekf@naver.com>
2023-08-03 02:31:07 +0900

    refactor : extract funtion to re-use

--- file_handler.py
+++ file_handler.py
@@ -29,19 +29,22 @@ class FileChangeHandler(FileSystemEventHandler):
         if not os.path.exists(event.src_path):
             return
         elif event.src_path.endswith(('.jpg', '.jpeg', '.png')):
-            new_file_path = event.src_path.replace(self.dir_path, self.new_dir_path)
-            try:
-                compress_image(event.src_path, new_file_path)
-            except Exception as e:
-                print(f'Error while compressing file {event.src_path}: {e}')
-                return
-            compress_time = datetime.now()
-            # log the event with timestamp
-            print(f'File {event.src_path} has been compressed to {new_file_path} '
-                  f'at {event.event_type} {compress_time}')
+            handle_file(event.src_path, event.src_path.replace(self.dir_path, self.new_dir_path))
 
-            # remove the original file
-            os.remove(event.src_path)
-            remove_time = datetime.now()
-            print(f'File {event.src_path} has been removed '
-                  f'at {event.event_type} {remove_time}')
+
+def handle_file(file_path: str, new_file_path: str) -> None:
+    try:
+        compress_image(file_path, new_file_path)
+    except Exception as e:
+        print(f'Error while compressing file {file_path}: {e}')
+        return
+    compress_time = datetime.now()
+    # log the event with timestamp
+    print(f'File {file_path} has been compressed to {new_file_path} '
+          f'at {compress_time}')
+
+    # remove the original file
+    os.remove(file_path)
+    remove_time = datetime.now()
+    print(f'File {file_path} has been removed '
+          f'at {remove_time}')
