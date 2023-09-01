2fa8c13bd95d968b2f3f8cb2f5356890e51732d1
JC Park <whdekf@naver.com>
2023-09-02 03:48:36 +0900

    REFACTOR : add performance test

--- tests/test_initial_file_handle.py
+++ tests/test_initial_file_handle.py
@@ -1,10 +1,11 @@
 import asyncio
 import os
+import shutil
 import time
 import unittest
-from distutils.dir_util import copy_tree
 
 import observer
+import task_handler
 
 
 class TestInitialFileHandle(unittest.TestCase):
@@ -24,7 +25,7 @@ class TestInitialFileHandle(unittest.TestCase):
         compressed_folder = current_path + os.sep + os.sep + 'test_images' + os.sep + 'compressed'
 
         # copy the files from tests/test_images/original to tests/test_images/uncompressed
-        copy_tree(original_folder, uncompressed_folder)
+        copy(original_folder, uncompressed_folder)
 
         # prepare the queue
         queue = asyncio.Queue()
@@ -33,10 +34,7 @@ class TestInitialFileHandle(unittest.TestCase):
         # measure the time it takes to run the initial_file_handle function
 
         start_time = time.perf_counter()
-
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(observer.initial_file_handle(uncompressed_folder, compressed_folder, que=queue))
-        loop.close()
+        asyncio.run(observer.initial_file_handle(uncompressed_folder, compressed_folder, que=queue))
 
         end_time = time.perf_counter()
         execution_time = end_time - start_time
@@ -55,6 +53,70 @@ class TestInitialFileHandle(unittest.TestCase):
         for root, dirs, files in os.walk(compressed_folder):
             for file in files:
                 os.remove(os.path.join(root, file))
+
+    def test_initial_file_handle_performance_test(self):
+        # given
+
+        # current path
+        current_path = os.getcwd()
+
+        # original folder for dummy images
+        original_folder = current_path + os.sep + 'test_images' + os.sep + 'original'
+
+        # uncompressed folder for storing the uncompressed images
+        uncompressed_folder = current_path + os.sep + 'test_images' + os.sep + 'uncompressed'
+
+        # compressed folder for the result
+        compressed_folder = current_path + os.sep + 'test_images' + os.sep + 'compressed'
+
+        # copy the files from tests/test_images/original to tests/test_images/uncompressed
+        copy(original_folder, uncompressed_folder)
+
+        # prepare the queue
+        queue = asyncio.Queue()
+
+        # when
+        # measure the time it takes to run the initial_file_handle function
+        aio_execution_time = task_handle(task_handler.TaskHandleType.aiomultiprocess, queue, uncompressed_folder, compressed_folder)
+
+        # change the mode to multi-process
+        copy(original_folder, uncompressed_folder)
+        mp_execution_time = task_handle(task_handler.TaskHandleType.multi_process, queue, uncompressed_folder, compressed_folder)
+
+        # change the mode to bounded semaphore
+        copy(original_folder, uncompressed_folder)
+        bs_execution_time = task_handle(task_handler.TaskHandleType.bounded_semaphore, queue, uncompressed_folder, compressed_folder)
+
+        # then
+        print(f'Execution time for aiomultiprocess: {aio_execution_time} seconds')
+        print(f'Execution time for multi process: {mp_execution_time} seconds')
+        print(f'Execution time for bounded semaphore: {bs_execution_time} seconds')
+        pass
+
+
+def task_handle(mode: task_handler.TaskHandleType, queue: asyncio.Queue, uncompressed_folder: str,
+                compressed_folder: str):
+    task_handler.mode = mode
+    start_time = time.perf_counter()
+
+    asyncio.run(observer.initial_file_handle(uncompressed_folder, compressed_folder, que=queue))
+
+    end_time = time.perf_counter()
+    execution_time = end_time - start_time
+    clean_up([uncompressed_folder, compressed_folder])
+
+    return execution_time
+
+
+def clean_up(dirs):
+    for dir in dirs:
+        for root, dirs, files in os.walk(dir):
+            for file in files:
+                os.remove(os.path.join(root, file))
+
+
+def copy(src, dst):
+    shutil.copytree(src, dst, dirs_exist_ok=True)
 
 
 if __name__ == '__main__':
